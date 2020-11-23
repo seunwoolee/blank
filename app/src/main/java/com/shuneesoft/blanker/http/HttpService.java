e163f8088a3a84cb3f21a9db76af8edf66760292
seunwoo <leemoney93@naver.com>
2020-11-23 15:55:17 +0900

    first commit

---
+++ app/src/main/java/com/shuneesoft/blanker/http/HttpService.java
@@ -0,0 +1,29 @@
+//package com.shuneesoft.blanker.http;
+//
+//
+//import com.example.jangbogovrp.model.RouteD;
+//import com.example.jangbogovrp.model.User;
+//
+//import java.util.List;
+//
+//import retrofit2.Call;
+//import retrofit2.http.Field;
+//import retrofit2.http.FormUrlEncoded;
+//import retrofit2.http.GET;
+//import retrofit2.http.POST;
+//import retrofit2.http.Query;
+//
+//public interface HttpService {
+////    @Headers({
+////            "Authorization: Token b553b4d63c2a1b679f4140f430da57198aec162b"
+////    })
+//    @GET("/delivery/android_routeD/")
+//    Call<List<RouteD>> getRouteDs(@Query("isAm") boolean isAm);
+//
+//    @FormUrlEncoded
+//    @POST("/rest-auth/login/")
+//    Call<User> login(@Field("username") String username, @Field("password") String password);
+//
+//}
+//
+//
---
+++ app/src/main/java/com/shuneesoft/blanker/http/RetrofitClient.java
@@ -0,0 +1,65 @@
+//package com.shuneesoft.blanker.http;
+//
+//import com.google.gson.Gson;
+//import com.google.gson.GsonBuilder;
+//
+//import org.jetbrains.annotations.NotNull;
+//
+//import java.io.IOException;
+//
+//import okhttp3.Interceptor;
+//import okhttp3.OkHttpClient;
+//import okhttp3.Request;
+//import okhttp3.Response;
+//import okhttp3.logging.HttpLoggingInterceptor;
+//import retrofit2.Retrofit;
+//import retrofit2.converter.gson.GsonConverterFactory;
+//
+//public class RetrofitClient {
+////    private static final String BASE_URL = "https://jangboso.co.kr/";
+//    private static final String BASE_URL = "http://192.168.0.58:8000/";
+//
+//
+//    public static HttpService getHttpService (String key) {
+//        return getInstance(key).create(HttpService.class);
+//    }
+//
+//    private static Retrofit getInstance(String key) {
+//        Gson gson = new GsonBuilder()
+//        .setLenient()
+//        .create();
+//
+//         Retrofit.Builder retrofitBuilder = new Retrofit.Builder()
+//                .baseUrl(BASE_URL)
+//                .addConverterFactory(GsonConverterFactory.create(gson));
+//
+//        HttpLoggingInterceptor logInterceptor = new HttpLoggingInterceptor();
+//        logInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+//
+//        Interceptor interceptor = new Interceptor(){
+//            @NotNull
+//            @Override
+//            public Response intercept(@NotNull Chain chain) throws IOException {
+//                Request newRequest = null;
+//                if(key == null) {
+//                    newRequest  = chain.request().newBuilder()
+//                            .build();
+//                } else {
+//                    newRequest  = chain.request().newBuilder()
+//                            .addHeader("Authorization", "Token " + key)
+//                            .build();
+//                }
+//                return chain.proceed(newRequest);
+//            }
+//        };
+//
+//        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+//        builder.addInterceptor(logInterceptor).addInterceptor(interceptor);
+//
+//        OkHttpClient client = builder.build();
+//        retrofitBuilder.client(client);
+//
+//        return retrofitBuilder.build();
+//    }
+//
+//}
